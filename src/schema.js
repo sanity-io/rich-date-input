212662118ca3f4829f5e01177b14e75073a71162
Thomas Drevon <thomas@bengler.no>
2017-10-19 13:39:49 +0200

    [form-builder] [schema] Add datetime type with default input (#275)
    * [base] Get rid of deprecatedDate type
    
    * [date-input] Rename common CSS file used by date input components
    
    * [schema] Enable the string-based date type
    
    * [date-input] Define string date part
    
    * [schema] RichDate type uses date internally
    
    * [date-input] Implement new string-based date type
    
    * [test-studio] Add string-based date type to test studio schema
    
    * [date-input] Include moment for a thinner Date.js
    
    * [date-input] Use moment without timezone db
    
    * [test-studio] Split dates schema
    
    * [date-input] Refactor out some common date methods to a shared file
    
    * [date-input] Move simple date input to form-builder and rename @santy/date-input to @sanity/rich-date-input
    
    * [base] Import richDate legacy schema from deprecated @sanity/date-input package
    
    * [form-builder] Add converter for richDate <=> datetime
    
    * [form-buidler] Remove inputUtc option
    
    * [form-builder] Disable keyboard navigation in date input
    
    * [form-builder] Simplify conversion from richDate to datetime

---
+++ src/schema.js
@@ -0,0 +1,31 @@
+import RichDateInput from './RichDate'
+
+export default {
+  title: 'Rich Date',
+  name: 'richDate',
+  type: 'object',
+  fields: [
+    {
+      name: 'utc',
+      type: 'datetime',
+      title: 'UTC',
+      required: true
+    },
+    {
+      name: 'local',
+      type: 'datetime',
+      title: 'Local'
+    },
+    {
+      name: 'timezone',
+      type: 'string',
+      title: 'Timezone'
+    },
+    {
+      name: 'offset',
+      type: 'number',
+      title: 'Offset'
+    }
+  ],
+  inputComponent: RichDateInput
+}
